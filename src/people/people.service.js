116e4eb19d377e108ee155a76f2ad3c6b76fbb2b
Mathew Murray <the.thran@gmail.com>
2020-11-06 14:27:44 -0500

    Update people.service.js

--- src/people/people.service.js
+++ src/people/people.service.js
@@ -1,13 +1,10 @@
 const Queue = require('../queue/Queue')
 const store = require('../store')
 
-// Set up initial data.
-// --------------------
 
 const people = new Queue()
 store.people.forEach(person => people.enqueue(person))
 
-// --------------------
 
 module.exports = {
   get() {
