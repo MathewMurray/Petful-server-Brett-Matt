b5c8a1e9a9479a5408565c011f2a28b7f31e55c0
Brett Westerlund <drbretto82@gmail.com>
2020-11-10 12:50:43 -0500

    deploying

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const config = require('../config')
-const app = express()
+const express = require("express");
+const config = require("../config");
+const app = express();
+const cors = require("cors");
+const helmet = require("helmet");
 
+app.use(helmet());
 app.use(cors());
 
-app.use('/people', require('../people/people.router'))
-app.use('/pets', require('../pets/pets.router'))
+app.use("/people", require("../people/people.router"));
+app.use("/pets", require("../pets/pets.router"));
 
-module.exports = app
+module.exports = app;
