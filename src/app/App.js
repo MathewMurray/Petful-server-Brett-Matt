d3ce2e9057132a8be8bf038bc7c2b76ca653f4a0
Brett Westerlund <drbretto82@gmail.com>
2020-11-10 12:34:29 -0500

    updated for cors

--- src/app/App.js
+++ src/app/App.js
@@ -3,9 +3,7 @@ const cors = require('cors')
 const config = require('../config')
 const app = express()
 
-app.use(cors({
-    origin: config.CLIENT_ORIGIN
-}));
+app.use(cors());
 
 app.use('/people', require('../people/people.router'))
 app.use('/pets', require('../pets/pets.router'))
